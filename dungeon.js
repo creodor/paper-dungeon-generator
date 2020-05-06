5c6fc7ad8060013faad14dddd4d4593a1ac710ef
creodor <1631686+creodor@users.noreply.github.com>
2020-05-05 22:41:33 -0500

    added some comments, made old map clear, removed test function

--- dungeon.js
+++ dungeon.js
@@ -2,34 +2,29 @@ function rollDice(sides, min = 1) {
     return Math.floor(Math.random() * (sides - min + 1)) + min; //we assume the minimum is always 1
 }
 
-function test() {
-    for (let i = 1; i < rollDice(6); i++){
-        var newTable = document.createElement("table");
-        var newContent = document.createTextNode("+");
-        newTable.appendChild(newContent);
-        var currentElem = document.getElementById("map");
-        document.body.insertBefore(newTable, currentElem);
-        console.log(i);
-    }
-};
-
-function mapTest() {
+function mapDisplay() {
+    //2d array to hold map information
+    //0 = mapCellSpace, ie walkable tile
+    //1 = mapCellWall, ie impassable tile
+    //will add further values for other options later
     var mapArray = [[1,1,1,1,1,1,1,1,1,1],[1,0,0,0,0,0,0,0,0,0],[1,1,1,1,0,1,1,1,0,1]];
     
     var currentElem = document.getElementById("mapDiv");
     var mapTable = document.getElementById("mapTable");
     var newRow = document.createElement("tr");
     var newCol = document.createElement("td");
+
+    //clears old map, if it exists
+    while(mapTable.firstChild) {
+        mapTable.removeChild(mapTable.firstChild);
+    }
     
+    //draw the map out
     for (let i = 0; i < mapArray.length; i++ ) {
-        //console.log(mapArray[i]);
-        
         var newRow = mapTable.insertRow(i);
         newRow.setAttribute("id", "mapRow");
 
         for (let j = 0; j < mapArray[i].length; j++) {
-            //console.log(mapArray[i][j])
-            
             var newCell = newRow.insertCell(j);
             if (mapArray[i][j] == 1) {
                 newCell.setAttribute("id", "mapCellWall");
